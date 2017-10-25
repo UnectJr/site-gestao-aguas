9a98df2c57a833970ac8a35cd57e34b3366aab93
Vinicius <vinicius0809@gmail.com>
2017-10-25 08:33:44 -0200

    Mantenha-me conectado e login por sessão

--- js/login.js
+++ js/login.js
@@ -12,19 +12,35 @@ var config = {
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
 const btnLogin = document.getElementById('btnLogin');
+const persistentLogin = document.getElementById('persistentLogin')
 
 //evento de login no botao
 btnLogin.addEventListener('click', e => {
     //pega email e password
     const email = txtEmail.value;
     const password = txtPassword.value;
-    const auth = firebase.auth();
+    console.log(persistentLogin.checked);
 
-    const promise = auth.signInWithEmailAndPassword(email,password);
-    promise.catch(e => {
-        window.alert("Usuário ou senha incorretos.");
-        console.log(e.message);  
-    }) 
+    if(persistentLogin.checked){
+        // Caixa de verificação marcada (login persistente)
+        const promise = firebase.auth().signInWithEmailAndPassword(email,password);
+        promise.catch(e => {
+            window.alert("Usuário ou senha incorretos.");
+            console.log(e.message);  
+        })  
+    }
+    else{
+        // Caixa de verificação não marcada (login por sessão)
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function() {
+        // Logout ao fechar a página
+        return firebase.auth().signInWithEmailAndPassword(email, password);
+        })
+        .catch(function(error) {
+            window.alert("Usuário ou senha incorretos.");
+            console.log(error.message); 
+        });
+    }
 })
 
 document.addEventListener('keypress', function(enter){
