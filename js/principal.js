6d4ef4cf0ce86be14ff1012397b211793df79d16
vinicius0809 <vinicius0809@gmail.com>
2018-04-06 09:26:19 -0300

    Firebase conta "appaguas@gmail.com"

--- js/principal.js
+++ js/principal.js
@@ -1,12 +1,12 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBiMgfyZ_izdFKLDl05vftWixxaS6xKIyM",
-  authDomain: "app-agua-utfpr.firebaseapp.com",
-  databaseURL: "https://app-agua-utfpr.firebaseio.com",
-  projectId: "app-agua-utfpr",
-  storageBucket: "app-agua-utfpr.appspot.com",
-  messagingSenderId: "142712018169"
-  };
+  apiKey: "AIzaSyAaiobOgYPey-2zhvXCt6XOyZutpBBpml4",
+  authDomain: "appaguas-utfpr.firebaseapp.com",
+  databaseURL: "https://appaguas-utfpr.firebaseio.com",
+  projectId: "appaguas-utfpr",
+  storageBucket: "",
+  messagingSenderId: "7612106731"
+};
 firebase.initializeApp(config);
 
 /********************************* resolvido_set *********************************/
